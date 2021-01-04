38d2ac404ccbb124b5b22a6e82fd4df34da40744
EC2 Default User <ec2-user@ip-172-31-10-34.us-east-2.compute.internal>
2021-01-04 12:48:08 +0000

    added tcGlobals

--- src/main.go
+++ src/main.go
@@ -1,32 +1,45 @@
+// Test Compliance Tool (TC)
 package main
 
 import (
 	"fmt"
 	"github.com/aseemsethi/tctool/src/inspector"
-	"github.com/aseemsethi/tctool/src/utils"
+	"github.com/aseemsethi/tctool/src/tcGlobals"
 )
 
+// All modules implement this interface
+// Using the Structural Component Pattern
 type tcIf interface {
 	Initialize()
 	Run()
 }
 
+// Main Control Struct for TC Tool
 type tc struct {
-	tcIfs []tcIf
+	//tcIfs []tcIf
+	tcIfs map[string]tcIf
 	name  string
 }
 
 func (tcNew tc) add(c tcIf) {
-	fmt.Printf("\nAdding a component to tc tool..")
-	tcNew.tcIfs = append(tcNew.tcIfs, c)
+	//fmt.Printf("\nAdding a component to tc tool..")
+	//tcNew.tcIfs = append(tcNew.tcIfs, c)
 }
 
 func main() {
 	fmt.Printf("\nTest Compliance Tool Starting..")
-	in := &inspector.Inspector{Name: "Inspector"}
+
 	tcTool := tc{name: "Test Compliance Tool"}
+	tcTool.tcIfs = make(map[string]tcIf)
+
+	tcg := &tcGlobals.TcGlobals{Name: "TC Globals"}
+	fmt.Printf("\nTC Tool - adding TC Globals Module")
+	tcTool.tcIfs["Globals"] = tcg
+
+	in := &inspector.Inspector{Name: "Inspector"}
 	fmt.Printf("\nTC Tool - adding Inspector Module")
-	tcTool.add(in)
-	utils.TestS3()
-	fmt.Printf("\nTC Tool - completed")
+	tcTool.tcIfs["Inspector"] = in
+
+	//utils.TestS3()
+	fmt.Printf("\nTC Tool - completed, %+v", tcTool)
 }
---
+++ src/tcGlobals/tcGlobals.go
@@ -0,0 +1,35 @@
+package tcGlobals
+
+import (
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+
+	"fmt"
+	"os"
+)
+
+type TcGlobals struct {
+	Name string
+	Sess session.Session
+}
+
+func (tcg *TcGlobals) Initialize() {
+	fmt.Printf("TcGlobals init..")
+
+	// Setup common session to be used by all Services
+	// Init session in us-east-2
+	sess, err := session.NewSession(&aws.Config{
+		Region: aws.String("us-east-2")},
+	)
+	if err != nil {
+		fmt.Println("Ecreating new session")
+		fmt.Println(err.Error())
+		os.Exit(1)
+	}
+
+	fmt.Println("TcGlobals: Session created..: %+v", sess)
+}
+
+func (tcg *TcGlobals) Run() {
+	fmt.Printf("TcGlobals run..")
+}
