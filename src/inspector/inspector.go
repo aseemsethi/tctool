f2f13da7978c08d383a66ab8753b3e6cfe6811ab
EC2 Default User <ec2-user@ip-172-31-89-172.ec2.internal>
2021-01-28 09:47:30 +0000

    Read region from env variables

--- src/inspector/inspector.go
+++ src/inspector/inspector.go
@@ -1,10 +1,10 @@
 package inspector
 
 import (
-	"fmt"
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
 	//"github.com/aws/aws-sdk-go/service/ec2"
+	//"fmt"
 	"github.com/aseemsethi/tctool/src/tcGlobals"
 	"github.com/aws/aws-sdk-go/service/inspector"
 	"github.com/sirupsen/logrus"
@@ -23,15 +23,23 @@ func (i *InspectorStruct) Initialize() bool {
 }
 
 func (i *InspectorStruct) Run() {
-	iLog.WithFields(logrus.Fields{
-		"Test": "CIS"}).Info("Inspector Run...")
-	sess, _ := session.NewSession()
-	//svc := inspector.New(sess)
+	iLog.WithFields(logrus.Fields{"Test": "CIS"}).Info("Inspector Run...")
+	sess, _ := session.NewSessionWithOptions(session.Options{
+		// Specify profile to load for the session's config
+		Profile: "default",
 
-	// Create a Inspector client with additional configuration
-	svc := inspector.New(sess, aws.NewConfig().WithRegion("us-east-1"))
+		// Provide SDK Config options, such as Region.
+		//Config: aws.Config{Region: aws.String("us-east-1")},
 
-	fmt.Println("1. CreateResourceGroupInput started")
+		// Force enable Shared Config support
+		SharedConfigState: session.SharedConfigEnable,
+	})
+	//_, err := sess.Config.Credentials.Get()
+	//fmt.Println("err: ", err)
+	svc := inspector.New(sess)
+
+	//svc := inspector.New(sess, aws.NewConfig().WithRegion("us-east-1"))
+
 	rgi := &inspector.CreateResourceGroupInput{
 		ResourceGroupTags: []*inspector.ResourceGroupTag{
 			{
@@ -40,12 +48,13 @@ func (i *InspectorStruct) Run() {
 			},
 		},
 	}
+	iLog.WithFields(logrus.Fields{"Test": "CIS"}).Info("Inspector ResGrp created")
 	rg, rgerr := svc.CreateResourceGroup(rgi)
 	if rgerr != nil {
-		fmt.Println(rgerr)
+		iLog.WithFields(logrus.Fields{"Test": "CIS"}).Info("Inspector ResGrp creation failed:", rgerr)
 		return
 	}
-	fmt.Println("Resource group: ", *rg.ResourceGroupArn)
+	iLog.WithFields(logrus.Fields{"Test": "CIS"}).Info("Inspector ResGrp created: ", *rg.ResourceGroupArn)
 
 	//return *rg.ResourceGroupArn
 
