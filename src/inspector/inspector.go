3c8335e7cd5ff35fc5dc0837ab82fa219c353cdd
EC2 Default User <ec2-user@ip-172-31-89-172.ec2.internal>
2021-01-28 05:25:58 +0000

    working on inspector

--- src/inspector/inspector.go
+++ src/inspector/inspector.go
@@ -2,7 +2,7 @@ package inspector
 
 import (
 	"fmt"
-	//"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
 	//"github.com/aws/aws-sdk-go/service/ec2"
 	"github.com/aseemsethi/tctool/src/tcGlobals"
@@ -26,6 +26,27 @@ func (i *InspectorStruct) Run() {
 	iLog.WithFields(logrus.Fields{
 		"Test": "CIS"}).Info("Inspector Run...")
 	sess, _ := session.NewSession()
-	svc := inspector.New(sess)
-	fmt.Println(svc)
+	//svc := inspector.New(sess)
+
+	// Create a Inspector client with additional configuration
+	svc := inspector.New(sess, aws.NewConfig().WithRegion("us-east-1"))
+
+	fmt.Println("1. CreateResourceGroupInput started")
+	rgi := &inspector.CreateResourceGroupInput{
+		ResourceGroupTags: []*inspector.ResourceGroupTag{
+			{
+				Key:   aws.String("inspector"),
+				Value: aws.String("true"),
+			},
+		},
+	}
+	rg, rgerr := svc.CreateResourceGroup(rgi)
+	if rgerr != nil {
+		fmt.Println(rgerr)
+		return
+	}
+	fmt.Println("Resource group: ", *rg.ResourceGroupArn)
+
+	//return *rg.ResourceGroupArn
+
 }
