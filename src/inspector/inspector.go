3a8c092559f2430f1fe8a9a8cdeba4de875fc2b0
Aseem Sethi <aseemsethi@yahoo.com>
2021-01-30 08:53:52 +0000

    working on inspector run

--- src/inspector/inspector.go
+++ src/inspector/inspector.go
@@ -96,9 +96,87 @@ func (i *InspectorStruct) Run() {
 		iLog.WithFields(logrus.Fields{"Test": "CIS"}).Info("Inspector Asessment Target ceration failed: ", aterr)
 		return
 	}
-	iLog.WithFields(logrus.Fields{"Test": "CIS"}).Info("Inspector Asessment Target created")
+	iLog.WithFields(logrus.Fields{"Test": "CIS"}).Info("Inspector Asessment Target created: ", at)
 	fmt.Println("AssessmentTarget: ", at)
-
 	//return *at.AssessmentTargetArn
 
+	// 3. create rules package input
+	rpi := &inspector.ListRulesPackagesInput{
+		MaxResults: aws.Int64(100),
+	}
+	rp, erp := svc.ListRulesPackages(rpi)
+	if erp != nil {
+		fmt.Println(erp.Error())
+		return
+	}
+	fmt.Println("List Rules Pkg: ", rp) // we selct all rules, i,e, N/W, CVE etc.
+
+	// 4. create assessment template
+	atli := &inspector.CreateAssessmentTemplateInput{
+		AssessmentTargetArn:    aws.String(*at.AssessmentTargetArn),
+		AssessmentTemplateName: aws.String("InspectorRun" + "_AssessmentTemplate_" + time.Now().Format("2006-01-02_15.04.05")),
+		DurationInSeconds:      aws.Int64(300),
+		RulesPackageArns:       rp.RulesPackageArns,
+		UserAttributesForFindings: []*inspector.Attribute{
+			{
+				Key:   aws.String("inspection-type"),
+				Value: aws.String("InspectorRun"),
+			},
+		},
+	}
+
+	atl, atlerr := svc.CreateAssessmentTemplate(atli)
+	if atlerr != nil {
+		fmt.Println(atlerr)
+		return
+	}
+	fmt.Println("Asessment Template: ", atl)
+
+	// 6. start assessment template run
+	ari := &inspector.StartAssessmentRunInput{
+		AssessmentRunName:     aws.String("InspectorRun" + "_Run_" + time.Now().Format("2006-01-02_15.04.05")),
+		AssessmentTemplateArn: aws.String(*atl.AssessmentTemplateArn),
+	}
+
+	ar, arerr := svc.StartAssessmentRun(ari)
+	if arerr != nil {
+		fmt.Println(arerr.Error())
+	}
+	fmt.Println("Asessment Run start: ", ar)
+	time.Sleep(360 * time.Second)
+
+	fmt.Println("Asessment Run complete: ")
+	input := &inspector.ListFindingsInput{
+		AssessmentRunArns: []*string{
+			aws.String(*ar.AssessmentRunArn),
+		},
+		MaxResults: aws.Int64(123),
+	}
+	list, err := svc.ListFindings(input)
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+	fmt.Println("Findings: ", list)
+
+	var resp []float64
+	for _, v := range list.FindingArns {
+		input := &inspector.DescribeFindingsInput{
+			FindingArns: []*string{
+				aws.String(*v),
+			},
+		}
+		fmt.Println("String: ", *v)
+
+		result, err := svc.DescribeFindings(input)
+		if err != nil {
+			fmt.Println(err.Error())
+			return
+		}
+		fmt.Println(*result.Findings[0].NumericSeverity)
+		fmt.Println(*result.Findings[0])
+		fmt.Println("\n")
+		resp = append(resp, *result.Findings[0].NumericSeverity)
+	}
+	fmt.Println("Findings: ", resp)
 }
